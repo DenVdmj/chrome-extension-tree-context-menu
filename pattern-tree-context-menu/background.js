e84f0629d638dde9510f7ab63df5befc5fb3ba38
DenVdmj <denvdmj@gmail.com>
2018-06-20 00:27:56 +0300

    just fix

--- pattern-tree-context-menu/background.js
+++ pattern-tree-context-menu/background.js
@@ -1,16 +1,16 @@
 
 const menuA = [
-  { id: 'ItemF', act: (info, tab) => { console.log('Clicked ItemF', info, tab, info.menuItemId); alert('Clicked ItemF') } },
-  { id: 'ItemG', act: (info, tab) => { console.log('Clicked ItemG', info, tab, info.menuItemId); alert('Clicked ItemG') } },
-  { id: 'ItemH', act: (info, tab) => { console.log('Clicked ItemH', info, tab, info.menuItemId); alert('Clicked ItemH') } },
-  { id: 'ItemI', act: (info, tab) => { console.log('Clicked ItemI', info, tab, info.menuItemId); alert('Clicked ItemI') } },
+  { id: 'ItemF', act: (info, tab) => { console.log('ItemF', info, tab, info.menuItemId); alert('ItemF') } },
+  { id: 'ItemG', act: (info, tab) => { console.log('ItemG', info, tab, info.menuItemId); alert('ItemG') } },
+  { id: 'ItemH', act: (info, tab) => { console.log('ItemH', info, tab, info.menuItemId); alert('ItemH') } },
+  { id: 'ItemI', act: (info, tab) => { console.log('ItemI', info, tab, info.menuItemId); alert('ItemI') } },
 ];
 
 const menuB = [
-  { id: 'ItemJ', act: (info, tab) => { console.log('Clicked ItemJ', info, tab, info.menuItemId); alert('Clicked ItemJ') } },
-  { id: 'ItemK', act: (info, tab) => { console.log('Clicked ItemK', info, tab, info.menuItemId); alert('Clicked ItemK') } },
-  { id: 'ItemL', act: (info, tab) => { console.log('Clicked ItemL', info, tab, info.menuItemId); alert('Clicked ItemL') } },
-  { id: 'ItemM', act: (info, tab) => { console.log('Clicked ItemM', info, tab, info.menuItemId); alert('Clicked ItemM') } },
+  { id: 'ItemJ', act: (info, tab) => { console.log('ItemJ', info, tab, info.menuItemId); alert('ItemJ') } },
+  { id: 'ItemK', act: (info, tab) => { console.log('ItemK', info, tab, info.menuItemId); alert('ItemK') } },
+  { id: 'ItemL', act: (info, tab) => { console.log('ItemL', info, tab, info.menuItemId); alert('ItemL') } },
+  { id: 'ItemM', act: (info, tab) => { console.log('ItemM', info, tab, info.menuItemId); alert('ItemM') } },
 ];
 
 
@@ -18,11 +18,11 @@ const rootMenu = [
   //
   // In real practice you must read chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT
   //
-  { id: 'ItemA', act: (info, tab) => { console.log('Clicked ItemA', info, tab, info.menuItemId); alert('Clicked ItemA') }, menu: menuA },
-  { id: 'ItemB', act: (info, tab) => { console.log('Clicked ItemB', info, tab, info.menuItemId); alert('Clicked ItemB') }, menu: menuB },
-  { id: 'ItemC', act: (info, tab) => { console.log('Clicked ItemC', info, tab, info.menuItemId); alert('Clicked ItemC') } },
-  { id: 'ItemD', act: (info, tab) => { console.log('Clicked ItemD', info, tab, info.menuItemId); alert('Clicked ItemD') } },
-  { id: 'ItemE', act: (info, tab) => { console.log('Clicked ItemE', info, tab, info.menuItemId); alert('Clicked ItemE') } },
+  { id: 'ItemA', menu: menuA },
+  { id: 'ItemB', menu: menuB },
+  { id: 'ItemC', act: (info, tab) => { console.log('ItemC', info, tab, info.menuItemId); alert('ItemC') } },
+  { id: 'ItemD', act: (info, tab) => { console.log('ItemD', info, tab, info.menuItemId); alert('ItemD') } },
+  { id: 'ItemE', act: (info, tab) => { console.log('ItemE', info, tab, info.menuItemId); alert('ItemE') } },
 ];
 
 
